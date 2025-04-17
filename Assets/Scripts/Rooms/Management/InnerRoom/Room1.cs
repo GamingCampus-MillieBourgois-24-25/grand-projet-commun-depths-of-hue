13e9a5bc3b2e59b529e13ccac9b4a3a768207ae6
CantineauFrancois <cantineaufrancois@hotmail.com>
2025-04-17 15:00:35 +0200

    Update Room1.cs

--- Assets/Scripts/Rooms/Management/InnerRoom/Room1.cs
+++ Assets/Scripts/Rooms/Management/InnerRoom/Room1.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public class Room1 : EnigmeRoom
 {
-    public override void Initialize()
-    {
-        base.Initialize();
-        Debug.Log("YALALALA");
-    }
-
-    protected override void OnPostEnigme()
-    {
-        if (IsRoomComplete())
-        {
-            EndRoomSequence();
-        }
-        else
-        {
-            FramesManager.Instance.LockFrame("Cave");
-        }
-    }
+    
 }
 
