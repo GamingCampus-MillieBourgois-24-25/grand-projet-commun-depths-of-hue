ab83d62319ac4730e851509c1e0861c8eccd1e8c
Arthur <104658619+PoloBongo@users.noreply.github.com>
2025-04-26 21:25:12 +0200

    Revert "Rope"

--- Assets/Scripts/UI/Map/AnimScale.cs
+++ Assets/Scripts/UI/Map/AnimScale.cs
@@ -7,24 +7,15 @@ public class AnimScale : MonoBehaviour
     [Header("Property")]
     [SerializeField] private ShowMap showMap;
     [SerializeField] private Animator animator;
-    
-    [Header("Audio")]
-    [SerializeField] private AudioSource audioSource;
 
     public void StartMap()
     {
         animator.SetTrigger(Open);
-        PlaySoundBtn();
     }
     
     public void ResetTrigger()
     {
         animator.ResetTrigger(Open);
         showMap.ClickMapIcon();
-    }
-
-    private void PlaySoundBtn()
-    {
-        audioSource.Play();
     }
 }
